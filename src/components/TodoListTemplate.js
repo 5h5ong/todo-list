c0172d7f17bdd77e2a986894892aeb0dd9d4c256
5h5ong <call5h5ong@gmail.com>
2018-12-17 08:46:55 +0900

    기본적인 구조 완성

---
+++ src/components/TodoListTemplate.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './TodoListTemplate.css'
+
+// 함수형 컴포넌트
+// 원래 Props로 받아야 하는 것을 비구조화 할당(destructuring assignment)을 이용해 ({form, children})로 받음.
+// 여러 종류의 JSX를 component로 넣어줄 때 편안한 방법. 이상 X
+const TodoListTemplate = ({form, children}) => {
+    return (
+        <main className="todo-list-template">
+            <div className="title">
+                
+                To-Do
+            </div>
+            <section className="form-wrapper">
+                {children}
+            </section>
+        </main>
+    );
+};
+
+export default TodoListTemplate;
