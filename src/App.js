79d539779e61df09f90b9b9352983a480a28694f
5h5ong <call5h5ong@gmail.com>
2018-12-17 18:38:47 +0900

    todo 삭제 구현

--- src/App.js
+++ src/App.js
@@ -75,6 +75,12 @@ class App extends Component {
             ]
         });
     }
+    handleRemove = (id) => {
+        const { todos } = this.state;
+        this.setState({
+            todos: todos.filter(todo => todo.id !== id)
+        });
+    }
 
     render() {
         const { input, todos } = this.state;
@@ -83,7 +89,8 @@ class App extends Component {
             handleChange,
             handleCreate,
             handleKeyPress,
-            handleToggle
+            handleToggle,
+            handleRemove
         } = this;
 
         return (
@@ -95,7 +102,7 @@ class App extends Component {
                     onCreate = {handleCreate}
                 />
             )}>
-                <TodoItemList todos={todos} onToggle={handleToggle}/>
+                <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
             </TodoListTemplate>
         );
     }
